22bfc8efacc7dcf7492ddc9726f4ab8e8c54c859
James McIntyre <james.checks.his.email@gmail.com>
2016-03-01 22:11:06 -0800

    more scaffolding and controller

--- src/js/module.js
+++ src/js/module.js
@@ -1,1 +1,10 @@
-var app = angular.module('myApp', []);
+'use strict';
+
+var app = angular.module('myApp', ['nvd3', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('main', { url: '/', templateUrl: '/partials/main.html', controller: 'mainCtrl' })
+
+  $urlRouterProvider.otherwise('/');
+});
